ce6863d944cbde9b343aff7ce667e97ed1f2426d
Mozamel Jawad <mozameljawad@gmail.com>
2023-11-15 12:50:40 +0430

    install devise and configure the user login and register pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
    # Defines the root path route ("/")
   # root "posts#index"
   # get "users/:user_id/posts", to: "posts#index", as: "user_posts"
   # get "users/:user_id/posts/:id", to: "posts#show", as: "user_post"
   # get "users", to: "users#index", as: "users"
   # get "users/:id", to: "users#show", as: "user"
+
+  root "users#index"
 
   resources :users do
     resources :posts do
