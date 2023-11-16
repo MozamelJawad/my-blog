46c8e684f0e4cbbe1421d7a7caafc0a36d8dcb73
Fatuma Hussein <fartunhussein346@gmail.com>
2023-11-16 10:45:09 +0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) do |u|
       u.permit(:name, :email, :bio, :photo, :password, :password_confirmation)
     end
+    
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :email, :bio, :photo, :password, :current_password, :role)
     end
+    
   end
 end
