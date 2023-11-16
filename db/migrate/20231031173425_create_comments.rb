7a01608530604a7846a1f72daaed41812f837450
Mozamel Jawad <mozameljawad@gmail.com>
2023-11-16 10:15:35 +0430

    Add role column and update table

--- db/migrate/20231031173425_create_comments.rb
+++ db/migrate/20231031173425_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
-      t.references :user, foreign_key: true
-      t.references :post, foreign_key: true
+      t.references :user, foreign_key:  { on_delete: :cascade, on_update: :cascade }
+      t.references :post, foreign_key:  { on_delete: :cascade, on_update: :cascade }
       t.text :text
       t.timestamps
     end
