90e1c3a6094f50b0f5a7b3e73ab4bad0ca3a8417
Fatuma Hussein <fartunhussein346@gmail.com>
2023-11-17 18:07:21 +0300

    Update comments_controller.rb

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,3 +1,8 @@
 class Api::CommentsController < ApplicationController
-
+skip_before_action :verify_authenticity_token
+  def index
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments
+    render json: @comments
+  end
 end
