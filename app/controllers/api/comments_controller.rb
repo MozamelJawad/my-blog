16b3d0b11142ffbd669b025726ce065760e586dd
Fatuma Hussein <fartunhussein346@gmail.com>
2023-11-17 18:07:42 +0300

    Update comments_controller.rb

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -5,4 +5,14 @@ skip_before_action :verify_authenticity_token
     @comments = @post.comments
     render json: @comments
   end
+   def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.author = current_user
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: {success: false, errors: @comment.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
